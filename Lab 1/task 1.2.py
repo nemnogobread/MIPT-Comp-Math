aaa11636f0929437e799adb549204aed3afc1ba2
Gleb <125480807+nemnogobread@users.noreply.github.com>
2023-09-25 20:27:08 +0300

    task 1.2 made, task 1.3 corrected a bit

--- Lab 1/task 1.2.py
+++ Lab 1/task 1.2.py
@@ -2,21 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import numpy.linalg as la
 
-def get_diff(u, l, m, h):
+def get_cond(u, l, m, h):
     n = u.size
     v = np.linspace(-l,m, n)
     # формируем матрицу системы в нужном виде
     A = np.fliplr(np.vander(v, v.size)).T 
-    b = np.zeros(n)
-    b[1] = 1
-    alpha = la.solve(A,b)
-    diff = 1/h*alpha.dot(u.T)
-    return diff
+    cond = la.cond(A)
+    return cond
 
 if __name__ == "__main__":
     p_list = [2**i for i in range(1, 7)]
-    diff_list = []
-    error_list = []
+    cond_list = []
 
     a = np.pi/3     # левая граница
     b = np.pi/2     # правая граница
@@ -26,13 +22,13 @@ if __name__ == "__main__":
         x = np.linspace(a, b, p+1)      # наложение сетки
         u = np.sin(x)       # значения синуса в точках сетки
         h = (b-a)/p     # шаг сетки
-        diff = get_diff(u, 0, p, h)     # считаем дифференциал в точке a
-        diff_list.append(diff)
-        error_list.append(abs(diff - 0.5))      # считаем ошибку как разность (sin(pi/3) = 0.5) 
+        cond = get_cond(u, 0, p, h)     # считаем дифференциал в точке a
+        cond_list.append(cond)
 
     #print(p_list)
-    #print(diff_list)
+    #print(cond_list)
+
     plt.xscale("log")
     plt.yscale("log")
-    plt.plot(p_list, error_list)
+    plt.plot(p_list, cond_list)
     plt.show()
